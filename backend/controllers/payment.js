95d3d61b3921667df5cc32d4f20f54eff4e25021
Matthew Spence <mspence5555@gmail.com>
2022-02-14 16:25:32 -0600

    can now make payment-intent req/res with stripe

--- backend/controllers/payment.js
+++ backend/controllers/payment.js
@@ -1,17 +1,38 @@
 const { default: Stripe } = require('stripe')
 const paymentManager = require('../public/javascripts/paymentManager')
-// const stripe = require('stripe')
-const stripe = require('stripe')('sk_test_51KSQNbAaTEmIXM6wt6O7rs6rMT1LttdkEP6NQBUtdHSIrilfukpYC6ZDT1vJRzbP6vHoOwbliaEwMnSA7JwJKpsV00sDkfdvxx'); // Add your Secret Key Here
+
+
+
 
 async function paymentIntent(req, res, next) {
 
-    const paymentIntention = await stripe.paymentIntention.create({
-        ammount: 1999,
-        currency: 'usd',
+    console.log("I should end up here^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    const stripe = require("stripe")('sk_test_51KSQNbAaTEmIXM6wt6O7rs6rMT1LttdkEP6NQBUtdHSIrilfukpYC6ZDT1vJRzbP6vHoOwbliaEwMnSA7JwJKpsV00sDkfdvxx');
+    
+    const calculateOrderAmount = (items) => {
+        // Replace this constant with a calculation of the order's amount
+        // Calculate the order total on the server to prevent
+        // people from directly manipulating the amount on the client
+        return 1400;
+      };
 
+      const { items } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 23 * 1000,// calculateOrderAmount(items),
+        currency: "usd",
+        // automatic_payment_methods: {
+        // enabled: true,
+        // },
     });
-    res.status(200).json(paymentIntention.client_secret)
-    // paymentManager.receivePayment();
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+
+    // res.status(200).json(paymentIntention.client_secret)
+    
 };
 
 exports.paymentIntent = paymentIntent
