8de31b4e9eb412e2a109812ea0130fac368e5f3a
Matthew Spence <mspence5555@gmail.com>
2022-02-18 14:14:17 -0600

    styling shopping cart, adding increase/decrease item ammount logic

--- frontend/src/components/Shoppingcrt.vue
+++ frontend/src/components/Shoppingcrt.vue
@@ -2,12 +2,21 @@
 <template>
 
     <p>Shopping Cart</p>
-    <div v-for="item in this.shoppingCart">
-        <div id="shoppingCartItems">
-            <p>{{ item.name }}</p>
-            <p>quantity: {{ item.buyQuantity }}</p>
-            <p>total price :{{ item.price * item.buyQuantity }}</p>
-        </div>
+
+    <div v-for="item in this.shoppingCart" id="shoppingCartItems" :data-item="item['id']">
+        
+        <p>{{ item.name }}</p>
+        <p>quantity: {{ item.buyQuantity }}</p>
+        <p>total price :{{ item.price * item.buyQuantity }}</p>
+
+        <form action="">
+            <div class="buyQtyBtnWrapper">
+                <button type="button" @click="decreaseItemQty">-</button>
+                <input id="buyQtyTxtInput" name="qtyInput" value="1" min="1" pattern="[0-9]*">
+                <button type="button" @click="increaseItemQty">+</button>
+            </div>
+        </form>
+
     </div> 
     
     <router-link to="/address">Checkout</router-link>
@@ -18,6 +27,7 @@
 // import axios from 'axios';
 import { defineComponent } from 'vue';
 import { globalState } from '../statestore/composition';
+
 export default defineComponent({
 
     setup() {
@@ -30,7 +40,7 @@ export default defineComponent({
         }
     },
 
-    
+
     mounted() {
         
    
@@ -38,7 +48,11 @@ export default defineComponent({
 
     methods: {
 
-       
+       decreaseItemQty(event) {
+
+           console.log(this.shoppingCart)
+           console.log(event)
+       }
     },
 
 })
@@ -47,9 +61,21 @@ export default defineComponent({
 
 </script>
 
-<style>
-    #shoppingCartItems {
+<style scoped>
+#shoppingCartItems {
 
-        border: 1px solid black;
-    }
+    border: 1px solid black;
+}
+.buyQtyBtnWrapper {
+
+display: flex;
+flex-direction: row;
+justify-content: center;
+
+}
+#buyQtyTxtInput {
+    text-align: center;
+    width: 35px;
+}
+
 </style>
