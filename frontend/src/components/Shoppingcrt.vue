ca8d24ebc8d3856aa1ebe466d6fea46724b28cb2
Matthew Spence <mspence5555@gmail.com>
2022-02-18 17:01:33 -0600

    calculate and display subtotal complete

--- frontend/src/components/Shoppingcrt.vue
+++ frontend/src/components/Shoppingcrt.vue
@@ -1,15 +1,14 @@
 
 <template>
 
-    <p>Shopping Cart</p>
-
+<div class="shoppingCartWrapper">
+    <h2 class="shoppingCartTitle">Shopping Cart</h2>
     <div v-for="item in this.shoppingCart" class="shoppingCartItems" :data-item="item['id']">
         
         <!-- only show cart items with positive quantity -->
         <div v-if="item['buyQuantity'] > 0" class="cartItem"> 
-            <p>{{ item.name }}</p>
+            <p class="cartItemName">{{ item.name }}</p>
             <form action="">
-      
                 <div class="qtyAdjustWrapper">
                     <p>quantity: {{ item.buyQuantity }}</p>
                     <div class="buyQtyBtnWrapper">
@@ -20,10 +19,15 @@
                 </div>
             <p>total price :{{ item.price * item.buyQuantity }}</p>
             </form>
-
         </div>
-    </div> 
-    
+    </div>
+    <div class="subTotal">
+
+        <p>Subtotal: ${{ subTotal }}</p>
+        
+
+    </div>
+</div>    
     <router-link to="/address">Checkout</router-link>
 </template>
 
@@ -45,13 +49,39 @@ export default defineComponent({
         }
     },
 
+    data() {
+
+        return{
+            subTotal: 0
+        }
+    },
+
 
     mounted() {
-        
+        this.calculateTotalCost();
    
     },
 
     methods: {
+
+        calculateTotalCost() {
+
+            // TODO validate ammount, set a env(?) variable for ammount and validate again
+            // right before payment
+            console.log(this.shoppingCart)
+            let ShoppingCartObj = this.shoppingCart
+            
+            // get subtotal
+            let subTotal = 0;
+            Object.keys(ShoppingCartObj).forEach(function (key){
+                subTotal += ShoppingCartObj[key]['price'] * ShoppingCartObj[key]['buyQuantity'];
+       
+                console.log(subTotal)
+            });
+            this.subTotal = subTotal;
+            console.log(subTotal)
+         
+        },
 
         decreaseItemQty(event) {
 
@@ -88,6 +118,19 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.shoppingCartTitle {
+
+    text-align: center;
+    font-weight: 800;
+
+}
+.shoppingCartWrapper {
+
+    display: flex;
+    flex-direction: column;
+    gap: 11px;
+}
+
 .shoppingCartItems {
 
     display: flex;
@@ -95,10 +138,14 @@ export default defineComponent({
     width: 100%;
 }
 .cartItem {
-
+    
     width: 80%;
     padding: 10px;
-    border: 1px solid black;
+    border-bottom: 1px solid #ccbdae;
+}
+.cartItemName {
+    text-align: left;
+    font-weight: 600;
 }
 .buyQtyBtnWrapper {
 
@@ -113,6 +160,17 @@ export default defineComponent({
 #buyQtyTxtInput {
     text-align: center;
     width: 35px;
+}
+.subTotal {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    border-bottom: 1px solid #ccbdae;
+    width: 100%;
+    padding: 10px;
+    margin-left: 10%;
+    
 }
 
 </style>
