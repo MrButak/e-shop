aad6a413fbdaf9e4f24086c65ec00128b3fbe596
Matthew Spence <mspence5555@gmail.com>
2022-02-22 12:16:43 -0600

    on payment success email is sent to the customer

--- backend/public/javascripts/webHookManager.js
+++ backend/public/javascripts/webHookManager.js
@@ -3,6 +3,7 @@ const email_sk = process.env.EMAIL_SK;
 
 exports.sendPaymentSuccessEmail = (paymentIntent) => {
 
+    // should I just pass these in directly to the params object?
     let customerName = paymentIntent.shipping.name;
     let streetAddress = paymentIntent.shipping.address.line1;
     let streetAddress2 = paymentIntent.shipping.address.line2;
@@ -11,12 +12,10 @@ exports.sendPaymentSuccessEmail = (paymentIntent) => {
     let postalCode = paymentIntent.shipping.address.postal_code;
     let country = paymentIntent.shipping.address.country;
     // TODO: would like to have items ordered in the metadata object when creating payment intent controllers/payment.js
-    let paymentAmount = paymentIntent.amount;
+    let paymentAmount = paymentIntent.amount / 100; // amount in cents
     let paymentStatus = paymentIntent.status;
     let email = paymentIntent.receipt_email;
-    
-    console.log(customerName, streetAddress, streetAddress2, city, state, postalCode, country, paymentAmount, email)
-    console.log("all this here ^^^^^^^^^^^^^^^^^^^^6")
+
    
     var SibApiV3Sdk = require('sib-api-v3-sdk');
     var defaultClient = SibApiV3Sdk.ApiClient.instance;
@@ -38,19 +37,17 @@ exports.sendPaymentSuccessEmail = (paymentIntent) => {
             email: email,
             name: customerName
         }],
-        templateId: 15,
+        templateId: 17,
         params: {
-
-            testy: "hey hey hey"
             
-            // customerName: customerName,
-            // streetAddress: streetAddress,
-            // streetAddress2: streetAddress2,
-            // city: city,
-            // state: state,
-            // postalCode: postalCode,
-            // country: country,
-            // paymentAmount: paymentAmount
+            customerName: customerName,
+            streetAddress: streetAddress,
+            streetAddress2: streetAddress2,
+            city: city,
+            state: state,
+            postalCode: postalCode,
+            country: country,
+            paymentAmount: paymentAmount
         },
         headers: {
             'X-Mailin-custom': 'api-key: email_sk|content-type: application/json|accept: application/json'
@@ -58,7 +55,7 @@ exports.sendPaymentSuccessEmail = (paymentIntent) => {
     };
     
     apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
-      console.log('API called successfully. Returned data: ' + data);
+      console.log('email API called successfully.'); //data
     }, function(error) {
       console.error(error);
     });
