bee066ea8ff67882f97344c3dec7bd73531b080d
Matthew Spence <mspence5555@gmail.com>
2022-03-12 08:15:08 -0600

    deploy

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const payment = require('../controllers/payment');
 const stripeWebHooks = require('../controllers/stripewebhooks');
 const validateForms = require('../controllers/validateforms');
 
-// allow requests from vue front end
+// allow requests from vue front end ** John, this may be the issue
 const allowRequest = app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
