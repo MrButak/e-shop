a3d929c534d43f4a445133947da1a4fa46ba57e0
Matthew Spence <mspence5555@gmail.com>
2022-03-13 00:03:47 -0600

    save point

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 var express = require('express');
-
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -11,9 +11,7 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
-const serveStatic = require("serve-static")
-const path = require('path');
-app.use(serveStatic(path.join(__dirname, 'public')));
+
 // this ensures I receive the raw header from stripe webhooks
 app.use('/webhook', bodyParser.raw({type: "*/*"}));
 
@@ -21,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
