b32b856eb0197c2bd0ad4d341090667fec28563d
Matthew Spence <mspence5555@gmail.com>
2022-03-13 00:20:08 -0600

    save point

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+// app.get(/.*/, function (req, res) {
+//     res.sendFile(path.join(__dirname, '/dist/index.html'))
+// })
 module.exports = app;
