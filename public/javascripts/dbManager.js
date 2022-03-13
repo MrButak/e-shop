a063dcf52e78dbfdc118bfe9ac889e6ba90dc806
Matthew Spence <mspence5555@gmail.com>
2022-03-13 16:57:48 -0500

    console.log db call

--- public/javascripts/dbManager.js
+++ public/javascripts/dbManager.js
@@ -12,8 +12,11 @@ client.connect();
 
 // Function gets all menu items from database
 exports.getMenu = async () => {
-  let res = await client.query('SELECT * FROM menu_items ORDER BY item_id ASC');
-  return res.rows;
+
+    console.log("can /menu/getmenu database call here ********************")
+
+    let res = await client.query('SELECT * FROM menu_items ORDER BY item_id ASC');
+    return res.rows;
 };
 
 // Function stores purchase information into database when receives stripe webhook for paymentIntent success
